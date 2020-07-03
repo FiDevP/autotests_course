6e2798115ed01fa64812bfc3c93a6c9825826357
Philipp Kachanov <fill2907@gmail.com>
2020-07-03 21:59:00 +0300

    the task with stepik

---
+++ par2_lesson4_step8.py
@@ -0,0 +1,28 @@
+from selenium import webdriver
+import math
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import time
+
+
+def calc(x):
+    return str(math.log(abs(12 * math.sin(int(x)))))
+
+link = "http://suninjuly.github.io/explicit_wait2.html"
+browser = webdriver.Chrome()
+browser.get(link)
+
+try:
+    WebDriverWait(browser,12).until(EC.text_to_be_present_in_element((By.ID, "price"),"$100"))
+    browser.find_element_by_id("book").click()
+    
+    number = browser.find_element_by_id("input_value").text
+    answer = calc(number)
+
+    browser.find_element_by_id("answer").send_keys(answer)
+    browser.find_element_by_id("solve").click()
+
+finally:    
+    time.sleep(10)
+    browser.quit()
